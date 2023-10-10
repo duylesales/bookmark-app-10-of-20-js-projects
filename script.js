9b86b4146f16809cf218cd4770d7c0a25bfc2d6f
Duy Node <duynode@Duys-MacBook-Pro.local>
2023-10-10 19:37:06 +0700

    Building the Bookmarks

--- script.js
+++ script.js
@@ -40,6 +40,41 @@ function validate(nameValue, urlValue) {
   return true;
 }
 
+// Build Bookmarks DOM
+function buildBookmarks() {
+  // Build items
+  bookmarks.forEach((bookmark) => {
+    const { name, url } = bookmark;
+    // Item
+    const item = document.createElement('div');
+    item.classList.add('item');
+    //Close Icon
+    const closeIcon = document.createElement('i');
+    closeIcon.classList.add('fas', 'fa-times');
+    closeIcon.setAttribute('title', 'Delete Bookmark');
+    closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+    // Favicon / Link Container
+    const linkInfo = document.createElement('div');
+    linkInfo.classList.add('name');
+    // Favicon
+    const favicon = document.createElement('img');
+    favicon.setAttribute(
+      'src',
+      `https://s2.googleusercontent.com/s2/favicons?domain=${url}`
+    );
+    favicon.setAttribute('alt', 'Favicon');
+    // Link
+    const link = document.createElement('a');
+    link.setAttribute('href', `${url}`);
+    link.setAttribute('target', '_blank');
+    link.textContent = name;
+    // Append to bookmarks container
+    linkInfo.append(favicon, link);
+    item.append(closeIcon, linkInfo);
+    bookmarksContainer.appendChild(item);
+  });
+}
+
 // Fetch Bookmarks
 function fetchBookmarks(e) {
   // Get bookmarks from localStorage if available
@@ -49,13 +84,13 @@ function fetchBookmarks(e) {
     // Create bookmarks array in localStorage
     bookmarks = [
       {
-        name: 'Yuki',
-        url: 'https;//yuki.com',
+        name: 'Thiên Đàng Mua Sắm',
+        url: 'https://thiendangmuasam.com',
       },
     ];
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   }
-  console.log(bookmarks);
+  buildBookmarks();
 }
 
 // Handle Data from Form
